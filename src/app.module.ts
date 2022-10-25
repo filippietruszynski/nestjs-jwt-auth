6693f437231e2c592346423177c2675d6792af05
Filip Pietruszyński <filippietruszynski@pm.me>
2022-10-25 15:09:18 +0200

    connect development and test databases

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Event as Szot } from './events/event.entity';
 
 @Module({
-  imports: [UsersModule, EventsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          entities: [User, Szot],
+          database: config.get<string>('DB_NAME'),
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          synchronize: config.get<boolean>('DB_SYNCHRONIZE'),
+        };
+      },
+    }),
+    UsersModule,
+    EventsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
